1fbf8f6b7955765906dfd6288e61ea33a594b7cd
AndersonPS94 <dson_adn@hotmail.com>
2025-09-09 08:13:36 -0300

    aulas sobre navegacao

--- app/src/main/java/com/example/mysecondcomposeapp/components/navigation/examples/LoginScreen.kt
+++ app/src/main/java/com/example/mysecondcomposeapp/components/navigation/examples/LoginScreen.kt
@@ -1,12 +1,18 @@
 package com.example.mysecondcomposeapp.components.navigation.examples
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,6 +20,9 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginScreen(navigateToDetail: () -> Unit) {
+
+    var showView: Boolean by remember { mutableStateOf(false) }
+
     Column(Modifier.fillMaxSize().background(Color.Blue), horizontalAlignment = Alignment.CenterHorizontally) {
         Spacer(Modifier.weight(1f))
         Text(text = "Login Screen", fontSize = 30.sp)
@@ -23,5 +32,12 @@ fun LoginScreen(navigateToDetail: () -> Unit) {
         }
         Spacer(Modifier.weight(1f))
 
+    }
+
+    if (showView) {
+        BackHandler {
+            showView = false
+        }
+        Box(Modifier.fillMaxSize().background(Color.Red))
     }
 }
